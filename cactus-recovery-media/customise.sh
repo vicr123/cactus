963b2af85b7b3f66a6c9fe27c8697bc5256f2977
Victor Tran <vicr12345@gmail.com>
2020-07-11 18:14:48 +1000

    Fix customise script

--- cactus-recovery-media/customise.sh
+++ cactus-recovery-media/customise.sh
@@ -6,5 +6,5 @@ sed -i 's/#autologin-session=/autologin-session=thedesk/' /etc/lightdm/lightdm.c
 sed -i 's/#greeter-session=example-gtk-gnome/greeter-session=lightdm-thedesk-greeter/' /etc/lightdm/lightdm.conf
 sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="loglevel=3 quiet"/GRUB_CMDLINE_LINUX_DEFAULT="loglevel=3 quiet splash"/' /etc/default/grub
 sed -i 's/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/' /etc/sudoers
-sed -i 's/HOOKS=(base udev autodetect modconf block filesystems keyboard fsck)/HOOKS=(base udev plymouth autodetect modconf block filesystems keyboard fsck)/' /etc/default/grub
+sed -i 's/HOOKS=(base udev autodetect modconf block filesystems keyboard fsck)/HOOKS=(base udev plymouth autodetect modconf block filesystems keyboard fsck)/' /etc/mkinitcpio.conf
 plymouth-set-default-theme -R bgrt-cactus
