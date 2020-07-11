bd2290f82393ebc22b5206d4420966a2f6832965
Victor Tran <vicr12345@gmail.com>
2020-07-11 18:17:46 +1000

    Add package repository

--- archiso/airootfs/root/customize_airootfs.sh
+++ archiso/airootfs/root/customize_airootfs.sh
@@ -16,5 +16,4 @@ sed -i "s/#Server/Server/g" /etc/pacman.d/mirrorlist
 
 systemctl enable plymouth-lightdm
 systemctl enable NetworkManager
-sed -i 's/HOOKS=(base udev autodetect modconf block filesystems keyboard fsck)/HOOKS=(base udev plymouth autodetect modconf block filesystems keyboard fsck)/' /etc/mkinitcpio.conf
 plymouth-set-default-theme -R bgrt-cactus
